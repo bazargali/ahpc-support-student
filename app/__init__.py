9d123c87e5b966835da03f610c8ea8d2f673dc76
bazargali <henryanri07@gmail.com>
2025-11-26 18:07:52 +0500

    Izmenenie

--- app/__init__.py
+++ app/__init__.py
@@ -44,8 +44,9 @@ def create_app():
     
     # Почта баптаулары
     app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
-    app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT') or 587)
+    app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT') or 465)
     app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS') == 'True'
+    app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL') == 'True' 
     app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
